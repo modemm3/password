2589ef107e1d2439d0e94ed611589580df565f61
modesto <modestom3@gmail.com>
2014-11-17 22:43:14 -0600

    changes

---
+++ src/main/java/com/cm/sva/datamart/gui/components/HideText.java
@@ -0,0 +1,27 @@
+package com.cm.sva.datamart.gui.components;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import org.edisoncor.gui.label.LabelTask;
+
+import com.cm.sva.datamart.dto.DTO;
+
+public class Label extends LabelTask {
+	private static final long serialVersionUID = 1L;
+	DTO o;
+
+	public Label(DTO o) {
+		super(o.getName());
+		this.o = o;
+	}
+
+	public DTO getO() {
+		return o;
+	}
+
+	public void setO(DTO o) {
+		this.o = o;
+	}
+	
+}
